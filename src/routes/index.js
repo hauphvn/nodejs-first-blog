647d1bc8e2cc536b977be05965f2a6d69fa9413a
Tee Pham <hauphvn@gmail.com>
2022-02-26 15:51:08 +0700

    test husky 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const newsRouter = require('./news');
 const otherRouter = require('./others');
 function route(app) {
   app.use('/news', newsRouter);
-      app.use('/', otherRouter);
+                 app.use('/', otherRouter);
 }
 
 module.exports = route;
